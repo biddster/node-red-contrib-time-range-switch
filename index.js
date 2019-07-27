dda997e28c294e6816e44f676ab9d06345382016
Stefan R <huzergackl@gmail.com>
2019-07-27 14:26:09 +0200

    #9 rewrite to allow placeholders cleanup of the logic added massive amount of unit tests

--- index.js
+++ index.js
@@ -24,33 +24,47 @@
  */
 
 module.exports = function(RED) {
-    'use strict';
 
-    const SunCalc = require('suncalc');
     const moment = require('moment');
-    const mustache = require("mustache");
     require('twix');
-    const fmt = 'YYYY-MM-DD HH:mm';
+
+    const PlaceholderParser = require('./placeholder-parser');
+    const DateParser = require('./date-parser');
 
     RED.nodes.registerType('time-range-switch', function(config) {
         RED.nodes.createNode(this, config);
 
         this.on('input', msg => {
+            const parser = new PlaceholderParser(this.context(), msg);
+
+            const startMoment = this.getParsedMoment(config.startTime, config.startOffset, parser);
+            const endMoment = this.getParsedMoment(config.endTime, config.endOffset, parser);
+
+            const isWithinRange = this.isCurrentTimeWithinRange(startMoment, endMoment);
+
+            this.sendMessage(msg, isWithinRange);
+            this.setStatus(startMoment, endMoment, isWithinRange);
+        });
+
+        this.now = function() {
+            return moment();
+        };
+
+        this.getParsedMoment = function(timeString, offset, placeholderParser) {
+            const parsedTime = placeholderParser.getParsedValue(timeString);
+            const parsedMoment = DateParser.momentFor(parsedTime, this.now());
+
+            const parsedOffset = placeholderParser.getParsedValue(offset);
+            if (parsedOffset) {
+                parsedMoment.add(parsedOffset, 'minutes');
+            }
+
+            return parsedMoment;
+        };
+
+        this.isCurrentTimeWithinRange = function(startMoment, endMoment) {
             const now = this.now();
-            const data = dataobject(this.context(), msg);
-            const startTime = mustache.render(config.startTime.toString(), data);
-            const startMoment = momentFor(startTime, now);
-            const endTime = mustache.render(config.endTime.toString(), data);
-            const endMoment = momentFor(endTime, now);
-            const startOffset = mustache.render(config.startOffset.toString(), data);
-            const endOffset = mustache.render(config.endOffset.toString(), data);
-            
-            if(startOffset) {
-                startMoment.add(startOffset, 'minutes');
-            }
-            if (endOffset) {
-                endMoment.add(endOffset, 'minutes');
-            }
+
             // align end to be before AND within 24 hours of start
             while (endMoment.diff(startMoment, 'seconds') < 0) {
                 // end before start
@@ -73,59 +87,32 @@ module.exports = function(RED) {
             }
 
             const range = moment.twix(startMoment, endMoment);
-            const output = range.contains(now) ? 1 : 2;
-            const msgs = [];
-            msgs[output - 1] = msg;
-            this.send(msgs);
+            return range.contains(now);
+        };
+
+        this.sendMessage = function(message, isWithinRange) {
+            const withinRangeOutput = 0;
+            const outsideOutput = 1;
+
+            const outputIndex = isWithinRange
+                ? withinRangeOutput
+                : outsideOutput;
+
+            const messageContainer = [];
+            messageContainer[outputIndex] = message;
+
+            this.send(messageContainer);
+        };
+
+        this.setStatus = function(startMoment, endMoment, isWithinRange) {
+            const startText = startMoment.format('HH:mm');
+            const endText = endMoment.format('HH:mm');
+
             this.status({
                 fill: 'green',
-                shape: output === 1 ? 'dot' : 'ring',
-                text: range.simpleFormat(fmt)
+                shape: isWithinRange ? 'dot' : 'ring',
+                text:  `${startText} - ${endText}`
             });
-        });
-
-        const momentFor = (time, now) => {
-            let m = null;
-            const matches = new RegExp(/(\d+):(\d+)/).exec(time);
-            if (matches && matches.length) {
-                m = now
-                    .clone()
-                    .hour(matches[1])
-                    .minute(matches[2]);
-            } else {
-                const sunCalcTimes = SunCalc.getTimes(now.toDate(), config.lat, config.lon);
-                const date = sunCalcTimes[time];
-                if (date) {
-                    m = moment(date);
-                }
-            }
-
-            if (m) {
-                m.seconds(0);
-            } else {
-                this.status({ fill: 'red', shape: 'dot', text: `Invalid time: ${time}` });
-            }
-            return m;
-        };
-
-        const dataobject = (context, msg) => {
-            var data = {}
-            data.msg = msg;
-            data.global = {};
-            data.flow = {};
-            var globalKeys = context.global.keys();
-            var flowKeys = context.flow.keys();
-            for (var key in globalKeys){
-                data.global[globalKeys[key]] = context.global.get(globalKeys[key]);
-            };
-            for (var key in flowKeys){
-                data.flow[flowKeys[key]] = context.flow.get(flowKeys[key]);
-            };
-            return data
-        };
-
-        this.now = function() {
-            return moment();
         };
     });
 };
